3d2b483c4b77512db296a8f156fab56b265d74f2
Tuan Anh 533 <22010371@st.phenikaa-uni.edu.vn>
2025-05-08 16:54:27 +0700

    Close #1: Viết các ca kiểm thử JUnit để bao phủ tất cả các lệnh trong chương trình

--- NumberCheckerTest.java
+++ NumberCheckerTest.java
@@ -29,19 +29,19 @@ public class NumberCheckerTest {
             System.out.println("Test số lẻ: FAILED");
         }
 
-        // Test Path Coverage: lẻ → chẵn → lẻ
-        int[] numbers = {1, 2, 3}; // Bao phủ đường đi: lẻ → chẵn → lẻ
-        out.reset();
-        checker.checkNumbers(numbers);
-        output = out.toString();
-        if (output.contains("1 là số lẻ") && 
-            output.contains("2 là số chẵn") && 
-            output.contains("3 là số lẻ")) {
-            System.out.println("Test Path Coverage: PASSED");
-        } else {
-            System.out.println("Test Path Coverage: FAILED");
-            // In ra kết quả thực tế để debug
-            System.out.println("Actual Output: " + output);
-        }
+        // // Test Path Coverage: lẻ → chẵn → lẻ
+        // int[] numbers = {1, 2, 3}; // Bao phủ đường đi: lẻ → chẵn → lẻ
+        // out.reset();
+        // checker.checkNumbers(numbers);
+        // output = out.toString();
+        // if (output.contains("1 là số lẻ") && 
+        //     output.contains("2 là số chẵn") && 
+        //     output.contains("3 là số lẻ")) {
+        //     System.out.println("Test Path Coverage: PASSED");
+        // } else {
+        //     System.out.println("Test Path Coverage: FAILED");
+        //     // In ra kết quả thực tế để debug
+        //     System.out.println("Actual Output: " + output);
+        // }
     }
 }
